432037a6028fb9bf6ab7575f3f6be861b52ecb68
suleman-uzair <suleman@workytical.com>
2026-08-20 18:08:46 +0500

    Regenerate formatting data

--- src/formatting/generated/provenance.ts
+++ src/formatting/generated/provenance.ts
@@ -47,7 +47,7 @@ export const FORMATTING_GENERATED_PROVENANCE: FormattingGeneratedProvenance = {
     ],
     [
       "scripts/generate-corpus.rb",
-      "5ade04ab709caebfc1baec2c63fbca8827218fe82258b0959ca51280543a63dd",
+      "a7c19cfe76254c834597df890a152f4783b02cefc95321eef0c00e60b7a1a1e6",
     ],
     [
       "scripts/generate-formatting-data.rb",

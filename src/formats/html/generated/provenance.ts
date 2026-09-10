f3407d0dc1a2d5f7821b3310a2d65222fa141562
suleman-uzair <suleman@workytical.com>
2026-09-10 15:43:22 +0500

    Rebind src/formats/html/generated

--- src/formats/html/generated/provenance.ts
+++ src/formats/html/generated/provenance.ts
@@ -48,7 +48,7 @@ export const HTML_PARSER_GENERATED_PROVENANCE: HtmlParserGeneratedProvenance = {
     ],
     [
       "scripts/generate-corpus.rb",
-      "a7ed6927c0f3a0821627de2bd6c2dc13e15dff889650419f588546b7ab8a1525",
+      "c2ce1875421d0281f778a3fcdf09c2d72ebc9acb91aef847a8b756407bd4bd69",
     ],
     [
       "scripts/generate-html-parser-data.rb",
